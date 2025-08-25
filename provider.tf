cf7bc5416f0ec54275939a79f345a72c0de39380
Bappa22 <bappakn22@gmail.com>
2025-08-25 06:16:56 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -19,6 +19,6 @@ backend "azurerm" {
 
 provider "azurerm" {
     features {}
-    subscription_id = "661ea49e-05d4-429e-8204-de0d46c539b5"
+    #subscription_id = "661ea49e-05d4-429e-8204-de0d46c539b5"
   
 }
